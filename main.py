3317b707aeaa9761960e22a68ab4d68bf2bc7540
Amanda <amandaismaili246@gmail.com>
2026-07-01 11:40:32 +0200

    adding routers for users and section

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from routers import users, section
+
 app = FastAPI()
+
+app.include_router(users.router, prefix="/user")
+app.include_router(section.router, prefix="/section")
 
 # cd "C:\Users\User\OneDrive\Desktop\studapp"
