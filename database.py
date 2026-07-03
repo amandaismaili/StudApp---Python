6751f304340bdcf696040db3fe23520e6ce15208
amandaismaili <amandaismaili246@gmail.com>
2026-07-03 11:07:09 +0200

    Database file

--- database.py
+++ database.py
@@ -1,6 +1,26 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-#from config import settings - add the ones with settings in it
+from config import settings 
+
+
+engine = create_async_engine(settings.database_url)
+
+
+AsyncSessionLocal = async_sessionmaker(
+    engine, 
+    class_ = AsyncSession,
+    expire_on_commit= False
+)
+
 
 class Base(DeclarativeBase):
     pass
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        except Exception:
+            await session.rollback()
+            raise
