70cc0eb5cde87bf9cd66e8e70467e2c70b467160
amandaismaili <amandaismaili246@gmail.com>
2026-07-08 12:13:48 +0200

    Changed in the database file

--- database.py
+++ database.py
@@ -2,9 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from sqlalchemy.orm import DeclarativeBase
 from config import settings 
 
-
 engine = create_async_engine(settings.database_url)
-
 
 AsyncSessionLocal = async_sessionmaker(
     engine, 
